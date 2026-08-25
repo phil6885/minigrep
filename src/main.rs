2894c8daa2dc7e7d38f4b7cc925ab272d9aa1bca
hongphil <phil6885@gmail.com>
2026-08-25 14:05:06 +0900

    ==== Refactor ==== 2.return a struct instead of tuple

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let (query, file_path) = parse_config(&args);
-    
+
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
 
-    (query, file_path)
+    Config { query, file_path }
 }

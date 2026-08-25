160fe8ae25f7053a9a47e0e17c6337b3a241b8db
hongphil <phil6885@gmail.com>
2026-08-25 15:18:53 +0900

    ==== Refactor ==== 3.Creating a Constructor for Config

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
     let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
@@ -12,6 +12,15 @@ fn main() {
 struct Config {
     query: String,
     file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config{
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
 
 fn parse_config(args: &[String]) -> Config {

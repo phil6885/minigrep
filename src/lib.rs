fb84f650136bc16a89fb81fa9ceb0cc9bf5dd5f2
hongphil <phil6885@gmail.com>
2026-08-26 12:19:11 +0900

    ==== Refactor ==== 12.Implement search function

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -34,9 +40,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
     }
 }

3398a47145ca2ccc75ce8ef97c3317be920c4fca
hongphil <phil6885@gmail.com>
2026-08-26 10:36:53 +0900

    ==== Refactor ==== 10.Splitting Code into a Library Crate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{env, fs, process};
-use std::error::Error;
+use std::{env, process};
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,38 +8,8 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{contents}");
-    Ok(())
 }
